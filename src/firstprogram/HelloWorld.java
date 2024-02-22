2e76e0506800be68d8bbfc991bf500432b1187d1
nuwan-dev <nuwanc.email@gmail.com>
2024-02-22 09:19:48 +0530

    firstJava

---
+++ src/firstprogram/HelloWorld.java
@@ -0,0 +1,9 @@
+package firstprogram;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        char grade = 'A';
+        String grad = "Kamal";
+        System.out.print(grad);
+    }
+}
